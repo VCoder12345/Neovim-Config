73f2fda5c83ce5269216cbaf6014202be7b8eed9
VCoder12345 <valentinlungershausen@hotmail.com>
2025-09-30 22:15:54 +0200

    autosessions and telescope changes

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -4,7 +4,6 @@ return {
     local auto_session = require("auto-session")
 
     auto_session.setup({
-      auto_restore_enabled = false,
     })
 
     local keymap = vim.keymap
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,28 +1,38 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files'}) 
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {desc = "Telescope buffers"})
 
-        end
+      -- Robust wrapper to prevent nil errors in lsp_document_symbols
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope diagnostics" })
+
+      vim.keymap.set("n", "<leader>fs", function()
+        builtin.lsp_document_symbols({
+          symbols = { "Class", "Struct", "Interface", "Function", "Method", "Constructor", "Trait" },
+        })
+      end, { desc = "Telescope lsp document symbols" })
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require("telescope").setup {
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-      }
-      }}}
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
 
       require("telescope").load_extension("ui-select")
-    end
-  }
+    end,
+  },
 }
