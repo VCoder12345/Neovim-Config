cde49e21d0e60180760b035d2262b032d8996c5d
Valentin Lungershausen <valentin.lungershausen@inf.ethz.ch>
2025-12-07 16:14:27 +0100

    some stuff

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -39,10 +39,11 @@ keymap.set("n", "<leader>P", '"+P', {desc = "paste from clipboard"})
 keymap.set("v", "<leader>p", '"+p', {desc = "paste from clipboard"})
 keymap.set("v", "<leader>P", '"+P', {desc = "paste from clipboard"})
 
+
 keymap.set("n", "ü", "^")
 keymap.set("v", "ü", "^")
 
--- making coding faster by replacing unused characters often used ones
+-- making coding faster by replacing unused characters by often used ones
 local feed = vim.api.nvim_feedkeys
 local function imap_insert(lhs, rhs)
     vim.keymap.set("i", lhs, function()
