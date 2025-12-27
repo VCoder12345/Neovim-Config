2e4f17fffede36fddfdd8006de049e816cf48c06
VCoder12345 <valentinlungershausen@hotmail.com>
2025-12-27 11:15:43 +0100

    folding

--- lua/dap/cpp.lua
+++ lua/dap/cpp.lua
@@ -2,7 +2,7 @@ local dap = require("dap")
 local install_root_dir = vim.fn.stdpath("data") .. "\\mason"
 local extension_path = install_root_dir .. "\\packages\\codelldb\\extension\\"
 local codelldb_path = extension_path .. "adapter\\codelldb"
-local last_cmd = vim.fn.getcwd() .. "\\"
+-- local last_cmd = vim.fn.getcwd() .. "\\"
 
 dap.adapters.codelldb = {
   type = "server",
@@ -10,23 +10,41 @@ dap.adapters.codelldb = {
   executable = {
     command = codelldb_path,
     args = { "--port", "${port}" },
+    timeout = 0,
 
     -- On windows you may have to uncomment this:
     detached = false,
   },
 }
+local last_cmd_file = vim.fn.getcwd() .. "/.last_exe"
+
 dap.configurations.cpp = {
   {
     name = "Launch file",
     type = "codelldb",
     request = "launch",
     program = function()
+      -- Try to read the last command from file
+      local last_cmd = vim.fn.getcwd() .. "\\" -- fallback
+      if vim.fn.filereadable(last_cmd_file) == 1 then
+        local lines = vim.fn.readfile(last_cmd_file)
+        if #lines > 0 then
+          last_cmd = lines[1]
+        end
+      end
+
+      -- Ask user for executable, defaulting to last_cmd
       local cmd = vim.fn.input("Path to executable: ", last_cmd, "file")
-      last_cmd = cmd
+
+      -- Save the selected command for next time
+      vim.fn.writefile({cmd}, last_cmd_file)
+
       return cmd
     end,
     cwd = "${workspaceFolder}",
     stopOnEntry = false,
+    console = "externalTerminal",
+    detached = false,
   },
 }
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,7 +16,7 @@ return {
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
-    vim.opt.foldcolumn = "1"
+    vim.opt.foldcolumn = "0"
     vim.opt.foldtext = ""
     vim.opt.foldlevel = 99
     vim.opt.foldlevelstart = 99
